c5cecff015beecd2c556d6b5c98501e32b9c1c7c
PK Consulting <zlodziej.koni@gmail.com>
2013-06-26 22:27:06 +0200

    tralal

---
+++ TransactionScoped/src/main/java/transactionscoped/PKCTest.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transactionscoped;
+
+/**
+ *
+ * @author pkconsulting
+ */
+public class PKCTest {
+    
+}
